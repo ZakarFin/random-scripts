2254e78f6aa83367464ab8511661d280439fc8a2
ZakarFin <zakarfin@gmail.com>
2014-07-06 22:15:02 +0300

    Messaging improvements

--- pebble/simplyjs/address.js
+++ pebble/simplyjs/address.js
@@ -1,8 +1,11 @@
 
+simply.text({ title: 'Address', body: 'Resolving... please wait' });
+
 function showResults(data) {
 	var msg = data.address.road + '\n' + 
+		data.address.suburb + ' / ' + data.address.city_district + '\n' + 
 		data.address.postcode + '  ' + data.address.city;
-    simply.text({ title: 'Address', body: msg });
+	simply.body(msg);
     simply.scrollable(true);
 }
 
@@ -15,7 +18,7 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   '&lon=' + coords.longitude;
   ajax({ url: reverseGeocodeUrl, type: 'json' }, showResults);
 },function(err) {
-	simply.body('Error getting location');
+	simply.body('Error resolving location');
 }, {
   enableHighAccuracy: true,
   timeout: 60000,
