d87cfb7603ca99a578a6e44d4f85da560ac8e9b3
ZakarFin <zakarfin@gmail.com>
2014-07-06 21:39:15 +0300

    var function perhaps not working on simplyjs?

--- pebble/simplyjs/address.js
+++ pebble/simplyjs/address.js
@@ -1,9 +1,10 @@
 
-var showResults = function(data) {
+function showResults(data) {
 	var msg = data.address.road + '\n' +
 		data.address.postcode data.address.city;
     simply.text({ title: data.display_name, subtitle: msg });
-};
+}
+
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
 //&json_callback=showResults
